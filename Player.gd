ed0b54935e443b78c9fe069472bcf0a6f46fca77
Bunny Blake <sonicforest@gmail.com>
2021-12-16 00:18:17 -0500

    real simple jumping

--- Player.gd
+++ Player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 const ACCELERATION = 240
 const MAX_SPEED = 90
 const FRICTION = 600
-
+const GRAVITY = 40
+const LIMIT_SPEED_Y = 100
+const JUMP_SPEED = 1000
 
 var velocity = Vector2.ZERO
 
@@ -12,13 +14,22 @@ var velocity = Vector2.ZERO
 # removing delta slows gameplay with frame drop (might be good?) but
 # completely changes the accelation / speed scale
 
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_Z:
+			velocity.y -= JUMP_SPEED    
+
 func _physics_process(delta):
 
+	if velocity.y < LIMIT_SPEED_Y:
+		velocity.y += GRAVITY
+		
 	# input vector
 	var _i = Vector2.ZERO
 	
 	_i.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	_i.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
 	
 	# normalize vector fixes fast/distorted diagonals
 	_i = _i.normalized()
