65b3f178c68cdd22f0800ed749c83af54e812610
Bunny Blake <sonicforest@gmail.com>
2022-01-06 21:20:41 -0500

    cliff dasher collision mysteries

--- Mobs/CliffDasher.gd
+++ Mobs/CliffDasher.gd
@@ -7,7 +7,7 @@ const DIVE_SPEED = 180
 
 var velocity : Vector2 = Vector2.ZERO
 var isDiving : bool = false
-var target = Player
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,12 +15,13 @@ func _ready():
 
 
 func on_SeekBox_body_entered(body: CollisionObject2D) -> void:
-	if body.get_name() != self.get_name() and !isDiving:
-		var _direction = ai_get_direction()
-		var _motion = _direction.normalized() * DIVE_SPEED
-		move_and_slide(_motion)
+	print("seen")
+	# var _direction = ai_get_direction()
+	#var _motion = _direction.normalized() * DIVE_SPEED
+	#move_and_slide(_motion)
 
 
 
 func ai_get_direction():
-	return target.position - self.position
+	# return target.position - self.position
+	pass
